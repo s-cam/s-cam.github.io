df8c5568419dce91ad5dad1fb8fa6493f431f712
s-cam <138610235+s-cam@users.noreply.github.com>
2023-07-12 21:02:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Navigation button click event listeners
-    const navButtons = document.querySelectorAll('.navigation-buttons li a');
-    navButtons.forEach(function(button) {
-        button.addEventListener('click', function(e) {
-            e.preventDefault();
-            const target = document.querySelector(button.getAttribute('href'));
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
+// Function to toggle a CSS class on an element
+function toggleClass(element, className) {
+  if (element.classList.contains(className)) {
+    element.classList.remove(className);
+  } else {
+    element.classList.add(className);
+  }
+}
+
+// Function to handle click events on the navigation menu
+function handleMenuClick(event) {
+  event.preventDefault();
+  var target = event.target;
+  
+  if (target.tagName === 'A' && target.classList.contains('menu-link')) {
+    var menu = target.closest('.menu');
+    var links = menu.querySelectorAll('.menu-link');
+    
+    links.forEach(function(link) {
+      link.classList.remove('active');
     });
-});
+    
+    target.classList.add('active');
+  }
+}
+
+// Function to initialize the script
+function init() {
+  var menu = document.querySelector('.menu');
+  menu.addEventListener('click', handleMenuClick);
+}
+
+// Initialize the script when the DOM is ready
+document.addEventListener('DOMContentLoaded', init);
